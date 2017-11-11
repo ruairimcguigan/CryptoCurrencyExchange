d1f4340234fd4307c5a9f658f4e142e5ff3f816b
Ruairi McGuigan <ruairi.mcguigan@gmail.com>
2017-11-11 13:51:26 +0000

    clearer project structure and dagger deps addedd

---
+++ app/src/main/java/exchange/currency/crypto/com/cryptocurrencyexchange/inject/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Contains the different custom scopes used.
+ * <p>
+ * Note that the standard {@link javax.inject.Singleton} scope is used as the application scope
+ * (there is no PerApplication scope annotation). This is done in order to support singleton
+ * scoped classes from other libraries (which do not have access to this project's custom scopes).
+ */
+package exchange.currency.crypto.com.cryptocurrencyexchange.inject;
---
+++ app/src/main/java/exchange/currency/crypto/com/cryptocurrencyexchange/ui/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * Contains all of the different UI components (activities and fragments) of the app.
+ */
+package exchange.currency.crypto.com.cryptocurrencyexchange.ui;
